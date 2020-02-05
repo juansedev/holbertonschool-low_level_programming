47b540d5c8c7e9a3239b84179f92f8a785a6cd79
Juan Sebastian <juanllano93@gmail.com>
2020-02-05 23:03:19 +0000

    add task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	printf("Size of a char:%c 1 byte(s)\n",sizeof(c));
+	return (0);
+}
