076dd6bad5209cd633cdeb50443e769e9660d513
Juan Sebastian <juanllano93@gmail.com>
2020-02-06 21:09:01 +0000

    add task 3 0x01 C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 97; n <=122; ++n)
+	for (n = 97; n < =122; ++n)
 	{
 		putchar(n);
 	}
-	for (n = 65; n <=90; ++n)
+	for (n = 65; n < =90; ++n)
 	{
 		putchar(n);
 	}
