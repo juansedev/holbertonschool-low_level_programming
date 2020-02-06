d38151519a37cad59b39d18334918bf83ca7c184
Juan Sebastian <juanllano93@gmail.com>
2020-02-06 20:52:03 +0000

    add task 2 0x01 C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the function main prints all alphabet
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; ++n)
+	{
+		putchar(n);
+	}
+	return (0);
+}
+
