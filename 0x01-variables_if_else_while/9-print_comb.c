4c6119175197ef1c107cb60cd7f9e69a13336238
Juan Sebastian <juanllano93@gmail.com>
2020-02-06 22:34:48 +0000

    add task 9 0x01 C

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the function main use printf to print numbers
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		printf("%d,", i);
+	}
+	printf("$\n");
+	return (0);
+}
+
