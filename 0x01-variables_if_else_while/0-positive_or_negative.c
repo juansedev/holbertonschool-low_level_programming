1ce894d605555c5461870097b7a5bf30bb2ca370
Juan Sebastian <juanllano93@gmail.com>
2020-02-06 18:43:51 +0000

    add task 0 of else nest it

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /*
- *In the function main be  generates a random number  a validate
- *if this number is positive, negative o zero
+ * In the function main be  generates a random number  a validate
+ * if this number is positive, negative o zero
  */
 int main(void)
 {
@@ -12,11 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf(" %d is positive", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf(" %d is negative", n);
+		printf("%d is negative\n", n);
 		else
-			printf(" %d is zero", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
-
