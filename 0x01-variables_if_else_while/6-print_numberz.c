32e09587805e120cad5aee942f336fca88b3e480
Juan Sebastian <juanllano93@gmail.com>
2020-02-06 22:18:40 +0000

    add task 6 0x01 C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the function main prints all alphabet
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; ++n)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
