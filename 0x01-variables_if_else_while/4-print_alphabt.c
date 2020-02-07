d4faa753f4e0b587c64abda2a5573217d70e203a
Juan Sebastian <juanllano93@gmail.com>
2020-02-07 04:56:08 +0000

    add task 4 again

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the function main prints all alphabet
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; ++n)
+	{
+		if ((n != 101) && (n != 113))
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
