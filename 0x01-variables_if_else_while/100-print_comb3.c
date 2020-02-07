cca63cf3bf28f5d57297e9043cfbfdda684ded7b
Juan Sebastian <juanllano93@gmail.com>
2020-02-07 03:51:06 +0000

    add task 100 0x01 C

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the function main use printf to print numbers
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	int u = 0;
+	int j = 49;
+
+	for (i = 48; i <= 56; ++i)
+	{
+		for (j ; j <= 57; ++j)
+		{
+
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		u++;
+		j = 49 + u;
+	}
+	putchar('\n');
+	return (0);
+}
+
