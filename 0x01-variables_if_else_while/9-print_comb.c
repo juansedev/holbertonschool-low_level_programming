ab994b858fb4e3043dde07c4813da86e74fdcc43
Juan Sebastian <juanllano93@gmail.com>
2020-02-07 00:36:51 +0000

    add task 9 0x01 C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,12 @@ int main(void)
 	for (i = 48; i <= 57; ++i)
 	{
 		putchar(i);
-		if(i != 57)
+		if (i != 57)
+		{
 			putchar(44);
 			putchar(32);
-
-	}	
+		}
+	}
 	putchar('\n');
 	return (0);
 }
