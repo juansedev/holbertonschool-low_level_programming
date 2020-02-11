968557419987663313fd1dac4ecb491cfbc26fcf
Juan Sebastian <juanllano93@gmail.com>
2020-02-11 15:19:29 +0000

    resolve problem with the print file 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+' + 0);
 		return (1);
-		_putchar('+');
 	}
 	else if (n < 0)
 	{
+		_putchar('-' + 0);
 		return (-1);
-		_putchar('-');
 	}
 	else
 	{
+		_putchar('0' + 0);
 		return (0);
-		_putchar('0');
 	}
 }
