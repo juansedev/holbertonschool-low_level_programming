0f063d0033f57c6e3f31f504eafb5fe3ba6850bc
Juan Sebastian <juanllano93@gmail.com>
2020-02-11 14:30:56 +0000

    add task 1 0x02 C Nestep

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  * print_alphabet -  this function print the alphabet
- *
+ * Descri: Entry
  * Return - void
  */
 void print_alphabet(void)
 {
 	int i = 0;
+
 	for (i = 97; i <= 122; ++i)
 	{
 		_putchar(i);
