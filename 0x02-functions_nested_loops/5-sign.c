a5bcdf80fe26681f6ef81c0c6ca0efb43e47b3c2
Juan Sebastian <juanllano93@gmail.com>
2020-02-11 14:32:19 +0000

    add task 1 and header file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar(43);
+		_putchar('+');
 	}
 	else if (n < 0)
 	{
 		return (-1);
-		_putchar(45);
+		_putchar('-');
 	}
 	else
 	{
 		return (0);
-		_putchar(48);
+		_putchar('0');
 	}
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,7 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* No doc is required for func prototypes */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -5,3 +9,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 
+#endif /* _HOLBERTON_H_ */
