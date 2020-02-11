a22f28c6a2b489702564e3ba2f2b7821053556f3
Juan Sebastian <juanllano93@gmail.com>
2020-02-11 17:57:04 +0000

    add task 7 0x02 C

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_last_digit - Print the last digif of a number
  * @n: The number to be cheked
@@ -9,20 +8,24 @@ int print_last_digit(int n)
 {
 	int last = 0;
 	int neg = -1;
-	printf("%d\n",n);
+	int limitIn = -2147483648;
+
 	if (n > 0)
 	{
 		last = n % 10;
 	} else if (n < 0)
 	{
-		if (n == -2147483648)
-			n = n + 1;
-		n = (n * neg);
-		printf("%d\n",n);
-		last = n % 10;
+		if (n == limitIn)
+		{
+			last = limitIn % 10;
+			last = last * neg;
+		} else
+		{
+			n = (n * neg);
+			last = n % 10;
+		}
 	} else
 		last = 0;
 	_putchar(last + '0');
-	_putchar('\n');
 	return (last);
 }
