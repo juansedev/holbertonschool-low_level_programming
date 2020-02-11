2c3820ac244295213e5e7b13b023a992292eba24
Juan Sebastian <juanllano93@gmail.com>
2020-02-11 17:15:45 +0000

    add task 7 0x02 C

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,12 @@ int print_last_digit(int n)
 	if (n > 0)
 	{
 		last = n % 10;
-	} else
+	} else if (n < 0)
 	{
 		n = n * -1;
 		last = n % 10;
-	}
+	} else
+		last = 0;
 	_putchar(last + '0');
 
 	return (last);
