4a30a19121e621f6731fd48fbf35dd025ffb1135
Juan Sebastian <juanllano93@gmail.com>
2020-02-12 23:47:19 +0000

    add task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,11 +8,8 @@
 
 void print_times_table(int n)
 {
-	int i, j, u, t, h;
-
-	h = n / 100;
-	t = (n % 100) / 10;
-	u = n % 10;
+	int i;
+	int j;
 
 	if (n > 0 && n <= 15)
 	{
@@ -30,40 +27,49 @@ void print_times_table(int n)
 /**
  * print_no_zero - No print tens  when is equal to '0'
  * @n: Number to ( i * j )
- * @col_a: Actual numbers columns @hun: hundred  @ten: tens  @unit: unit  @n:
+ * @col_act: Actual numbers columns
  * @limit: max value to columns
  * Return: Void
  */
-void print_no_zero(int hun, int ten, int unit, int n, int col_a, int limit)
+
+void print_no_zero(int n, int col_act, int limit)
 {
+	int units, tens, hundreds;
+
+	hundreds = n / 100;
+	tens =  (n % 100) / 10;
+	units = n % 10;
+
 	if (n > 99)
 	{
-		if (col_a > 0)
+		if (col_act > 0)
 			_putchar(32);
-		_putchar(hun + '0');
-		_putchar(ten + '0');
-		_putchar(unit + '0');
+		_putchar(hundreds + '0');
+		_putchar(tens + '0');
+		_putchar(units + '0');
 	}
 	if ((n > 9) && (n <= 99))
 	{
-		if (col_a > 0)
+		if (col_act > 0)
 		{
 			_putchar(32);
 			_putchar(32);
 		}
-		_putchar(ten + '0');
-		_putchar(unit + '0');
+		_putchar(tens + '0');
+		_putchar(units + '0');
 	}
 	if (n <= 9)
 	{
-		if (col_a > 0)
+		if (col_act > 0)
 		{
 			_putchar(32);
 			_putchar(32);
 			_putchar(32);
 		}
-		_putchar(unit + '0');
+		_putchar(units + '0');
 	}
-	if (col_a < limit)
+	if (col_act < limit)
+	{
 		_putchar(44);
+	}
 }
