fe36aeebef96a600ab89d20a014038591d8e8e1d
Juan Sebastian <juanllano93@gmail.com>
2020-02-12 23:41:03 +0000

    add task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,11 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j, u, t, h;
+
+	h = n / 100;
+	t = (n % 100) / 10;
+	u = n % 10;
 
 	if (n > 0 && n <= 15)
 	{
@@ -27,49 +30,40 @@ void print_times_table(int n)
 /**
  * print_no_zero - No print tens  when is equal to '0'
  * @n: Number to ( i * j )
- * @col_act: Actual numbers columns
+ * @col_a: Actual numbers columns @hun: hundred  @ten: tens  @unit: unit  @n:
  * @limit: max value to columns
  * Return: Void
  */
-
-void print_no_zero(int n, int col_act, int limit)
+void print_no_zero(int hun, int ten, int unit, int n, int col_a, int limit)
 {
-	int units, tens, hundreds;
-
-	hundreds = n / 100;
-	tens =  (n % 100) / 10;
-	units = n % 10;
-
 	if (n > 99)
 	{
-		if (col_act > 0)
+		if (col_a > 0)
 			_putchar(32);
-		_putchar(hundreds + '0');
-		_putchar(tens + '0');
-		_putchar(units + '0');
+		_putchar(hun + '0');
+		_putchar(ten + '0');
+		_putchar(unit + '0');
 	}
 	if ((n > 9) && (n <= 99))
 	{
-		if (col_act > 0)
+		if (col_a > 0)
 		{
 			_putchar(32);
 			_putchar(32);
 		}
-		_putchar(tens + '0');
-		_putchar(units + '0');
+		_putchar(ten + '0');
+		_putchar(unit + '0');
 	}
 	if (n <= 9)
 	{
-		if (col_act > 0)
+		if (col_a > 0)
 		{
 			_putchar(32);
 			_putchar(32);
 			_putchar(32);
 		}
-		_putchar(units + '0');
+		_putchar(unit + '0');
 	}
-	if (col_act < limit)
-	{
+	if (col_a < limit)
 		_putchar(44);
-	}
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -12,7 +12,7 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-void print_no_zero(int n, int col_act, int limit);
+void print_no_zero(int hundreds, int tens, int units, int n, int col_act, int limit);
 int add(int n, int m);
 void print_to_98(int n);
 void print_times_table(int n);
