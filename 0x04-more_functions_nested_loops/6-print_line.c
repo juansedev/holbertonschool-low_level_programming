f843f35da7b4452a0749c7f117b8df27c460a396
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 21:02:30 +0000

    add task 6 0x04 C 0x04

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,12 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
-	} 
+	}
 	else
 		_putchar('\n');
 }
