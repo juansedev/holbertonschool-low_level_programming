de7a3ea15866c300f5dc3b6d392f6408eefb9e64
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 20:46:11 +0000

    add task 6 0x04 C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_line - print character (_) n times
+ * @n: Indicates the number of times
+ * Return: Void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if ( n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	} else 
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif /* HOLBERTON_H */
