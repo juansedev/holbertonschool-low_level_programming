a6dc2e36e11a3382dc3bfdc772961b9fcd174605
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 18:40:10 +0000

    add task 2 0x04 C more loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two digit
+ * @a: digit one
+ * @b: digit two
+ * Return: Result to multiplies
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
