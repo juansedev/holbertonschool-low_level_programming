5dcab2dcd2444dc55b9d43a40c6876054b8ae1e9
Juan Sebastian <juanllano93@gmail.com>
2020-02-12 23:27:37 -0500

    add task advance 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - function recursive to calculate n fibonacci
+ * @numero: numero to be generate serie
+ * Return: numero
+ */
+
+long fibonacci(long numero)
+{
+	long a;
+
+	if (numero == 0 || numero == 1)
+	{
+		return (numero);
+	}
+	else
+	{
+		a = (fibonacci(numero - 1) + fibonacci(numero - 2));
+		return (a);
+	}
+}
+
+
+/**
+ * main - Entry point function fibonacci
+ *
+ * Return: (0) Succes
+ */
+
+int main(void)
+{
+	long resultado, num;
+
+	num = 1;
+	while (num <= 50)
+	{
+		resultado = fibo(num);
+		printf("%ld, ", resultado);
+		num++;
+	}
+	return (0);
+}
