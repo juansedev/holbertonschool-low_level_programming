803396b2d049445e25f51753d830ab8824b0c49c
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 19:15:54 +0000

    add task 4 0x04 C nestep loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - print the numbers
+ *
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if ( i != 50 && i != 52)
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,5 +7,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char c);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* HOLBERTON_H */
