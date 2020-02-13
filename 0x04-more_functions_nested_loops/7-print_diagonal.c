22fb9b85adf4e0008b2faca7ff766603528fdd09
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 22:00:35 +0000

    add task 7 0x04 C

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal signal
+ * @n: value to create matriz
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+					_putchar('\\');
+				else
+					_putchar(32);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
