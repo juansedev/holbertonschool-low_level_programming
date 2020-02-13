2d678c7d3dd06a6f1356d5b44494cfa49e569197
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 19:48:03 +0000

    add task 5 0x04 C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * more_numbers - print the numbers
+ *
+ * Return: Void
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j, tens, units;
+
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			tens = j / 10;
+			units = j % 10;
+			if (tens > 0)
+				_putchar(tens + '0');
+			_putchar(units + '0');
+			if (j == 14)	
+				_putchar('\n');
+		}
+		i++;
+	}
+}
