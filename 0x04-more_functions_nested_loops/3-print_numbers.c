905a331abc9aaabd942167b37cffb017d56b1911
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 19:07:55 +0000

    add task 03 0x04 C nestep loops more

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_numbers - print the numbers
+ *
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
