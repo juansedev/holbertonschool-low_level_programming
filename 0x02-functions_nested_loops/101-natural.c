4ad9ba87c3971ec2913815d75a7b7d7f1c844bd2
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 00:05:30 +0000

    add task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - add when i is multiples of 3 or 5
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	int i = 0, add = 0;
+
+	for ( ; i < 1024; ++i)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			add += i;
+		}
+	}
+	printf("%d \n", add);
+	return (0);
+}
