833e02479232c0928ed7f1f86652480804e00168
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 23:38:33 -0500

    add task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
 			if ((i % 3) == 0)
-				printf("Fizz");
+				printf("Fizz ");
 			else
 				if ((i % 5) == 0)
-					printf("Buzz");
+					printf("Buzz ");
 				else
-					printf("%d", i);
-		printf(" ");
+					printf("%d ", i);
 	}
 	printf("\n");
+
 	return (0);
 }
