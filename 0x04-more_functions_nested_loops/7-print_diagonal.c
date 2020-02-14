2b6afdeccc0923452124c48963ed2814f6219adc
Juan Sebastian <juanllano93@gmail.com>
2020-02-13 21:42:20 -0500

    add task 7 0x04 C

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,17 @@
 void print_diagonal(int n)
 {
 	int i, j;
+
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
+				if (i > j)
+					_putchar('.');
 				if (i == j)
 					_putchar('\\');
-				else
-					_putchar(32);
 			}
 			_putchar('\n');
 		}
