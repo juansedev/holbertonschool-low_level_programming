9d0d69ffcfcf1bcc145f412075175affdda5b5bb
Juan Sebastian <juanllano93@gmail.com>
2020-02-18 22:54:31 +0000

    add task 8

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -12,8 +12,9 @@ int main(void)
 	array[0] = 98;
 	array[1] = 402;
 	array[2] = -198;
-	array[3] = 298;
-	array[4] = -1024;
+	array[3] = -2147483648;
+	array[4] = 2147483647;
 	print_array(array, 5);
+
 	return (0);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -32,7 +32,7 @@ void print_array(int *a, int n)
 	{
 		for ( ; i < n; i++)
 		{
-			printf("%d",*(a + i));
+			printf("%d", *(a + i));
 			if (i < (n - 1))
 				printf(", ");
 		}
