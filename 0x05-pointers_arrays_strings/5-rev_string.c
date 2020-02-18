6379a15272ba898e4d9eeadc7da656cc7390b031
Juan Sebastian <juanllano93@gmail.com>
2020-02-18 18:18:59 +0000

    add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	int lenght = 0, swap = 0;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,6 +4,12 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+<<<<<<< HEAD
 void rev_string(char *s);
+=======
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+>>>>>>> a18f4796cd0298428c5ed60db87a81e883e346d3
 
 #endif /* _HOLBERTON_H_ */
