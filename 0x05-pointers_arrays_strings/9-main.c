284d044eef21ec4285ea08368dc0bf1f96d05349
Juan Sebastian <juanllano93@gmail.com>
2020-02-19 03:02:27 +0000

    add task 9

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -11,7 +11,7 @@ int main(void)
 	char s1[98];
 	char *p;
 
-	p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	p = _strcpy(s1, "First, solve the Then, wde\n");
 	printf("%s", s1);
 	printf("%s", p);
 	return (0);
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,21 +8,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0, i = 0;
+	int i = 0;
 
 	if (src != '\0')
 	{
-		while (*(src + len) != '\0')
-		{
-			len++;
-		}
-
-		for (i = 0; i < (len - 1); i++)
+		for (i = 0;  *(src + i) != '\0'; i++)
 		{
 			*(dest + i) = *(src + i);
 		}
 	}
 
-		*(dest + i) = *(src + i);
+	*(dest + i) = '\0';
 	return (dest);
 }
