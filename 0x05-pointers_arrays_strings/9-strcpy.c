66235a6d0ade5aaca05c28c21547275c8e376b2d
Juan Sebastian <juanllano93@gmail.com>
2020-02-19 00:44:11 +0000

    add task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,11 +16,14 @@ char *_strcpy(char *dest, char *src)
 		len++;
 	}
 
-	for (i = 0; i < (len - 1); i++)
+	if (len > 0)
 	{
+		for (i = 0; i < (len - 1); i++)
+		{
+			*(dest + i) = *(src + i);
+		}
 		*(dest + i) = *(src + i);
 	}
-	*(dest + i) = *(src + i);
 
 	return (src);
 }
