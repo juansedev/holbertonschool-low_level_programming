a87de078e258fff25b11998bb3d79a8e93636107
Juan Sebastian <juanllano93@gmail.com>
2020-02-22 00:32:15 +0000

    add task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to upeercase
+ * @a: char
+ * Return: char
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\n')
+	{
+		if ( a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
