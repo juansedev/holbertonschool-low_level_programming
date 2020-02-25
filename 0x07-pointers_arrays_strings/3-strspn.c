a07f7471203025dd1b9e2683dc1e0fdee95b5a44
Juan Sebastian <juanllano93@gmail.com>
2020-02-25 01:00:28 +0000

    add task 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,10 +11,10 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, stop;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; *(s + i + 1) ; i++)
 	{
 		stop = 1;
-		for (j = 0; *(accept + j); j++)
+		for (j = 0; *(accept + j + 1); j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
