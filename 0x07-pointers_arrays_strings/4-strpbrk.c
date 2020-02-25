87cf2f5c5d3ba760921f9a0be4fb75e071fb63f3
Juan Sebastian <juanllano93@gmail.com>
2020-02-25 01:02:56 +0000

    add task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,18 +12,18 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int i, j, stop;
 	char *tmp;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; *(s + i + 1); i++)
 	{
 		stop = 0;
-		for (j = 0; *(accept + j); j++)
+		for (j = 0; *(accept + j + 1); j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
 				tmp = s + i;
 				stop = 1;
 				break;
-			} else
-				tmp = 0;
+			}
+			tmp = 0;
 		}
 		if (stop == 1)
 			break;
