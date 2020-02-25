cd05f3dac215fd713e8e4d3bdcddf19670ea64a5
Juan Sebastian <juanllano93@gmail.com>
2020-02-25 04:01:43 +0000

    add task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	for (; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(*(*(a + i)+ j));
 		}
 		_putchar('\n');
 	}
