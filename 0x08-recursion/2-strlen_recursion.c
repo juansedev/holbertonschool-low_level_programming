4195b0f34cacef1f0e0a71674c97f239e972b746
Juan Sebastian <juanllano93@gmail.com>
2020-02-26 22:15:34 +0000

    add task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - function that print lenght a string
+ * @s: string to print recursively
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	int lenght = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	lenght = 1 + _strlen_recursion((s + 1));
+	return (lenght);
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -4,5 +4,6 @@
 void _puts_recursion(char *s);
 void _putchar(int c);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif /* _HOLBERTON_H_ */
