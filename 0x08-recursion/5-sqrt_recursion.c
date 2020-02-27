f976037cccf0b0eaacb2084a011b2f00ff76fe8c
Juan Sebastian <juanllano93@gmail.com>
2020-02-27 00:47:50 +0000

    add task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - function that returns the natural square root of a number
+ * _sqrt_recursion - function that returns the natural square root of
+ * a number
  * @n: number to calculate the sqrt
  * Return: Value to sqrt
  */
@@ -16,18 +17,19 @@ int _sqrt_recursion(int n)
 		return (result);
 
 }
-
+/**
+ * calculate_sqrt - function that calculate the natural square root of
+ * a number
+ * @n: number to calculate sqrt
+ * @i: initial number to test sqrt
+ * Return: Value to sqrt
+ */
 int calculate_sqrt(int  n, int i)
 {
 	if (n / i == i && n % i == 0)
 		return (i);
-	else
-		if (n / i < i)
-			return (-1);
-		else
-		{
-			i = calculate_sqrt(n, i + 1);
-			return (i);
-
-		}
+	if (n / i < i)
+		return (-1);
+		i = calculate_sqrt(n, i + 1);
+		return (i);
 }
