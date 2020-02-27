fce9195776cfa119d3cc3ea780b9e97bb1ef1c54
Juan Sebastian <juanllano93@gmail.com>
2020-02-27 02:25:11 +0000

    add task 6

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int calculate_sqrt(int n, int i);
+int calculate_prime(int n, int i);
+int is_prime_number(int n);
 
 #endif /* _HOLBERTON_H_ */
