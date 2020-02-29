9e4875450c395ead2d63d74d16a92164084fc87e
Juan Sebastian <juanllano93@gmail.com>
2020-02-29 20:44:43 +0000

    add task 0

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -4,7 +4,7 @@
 int _isupper(int c);
 char *_memset(char *s, char b, unsigned int n);
 char *_strcat(char *dest, char *src);
-int _atoi(char *s);int _atoi(char *s);
+int _atoi(char *s);
 int _isdigit(int c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strncat(char *dest, char *src, int n);
