9bf14d85cbc86df3ca466136b9c886b5f9561433
Juan Sebastian <juanllano93@gmail.com>
2020-02-29 22:22:14 -0500

    add task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,10 @@ int main(int argc, char *argv[])
 {
 	(void) argc;
 	if (*(argv + 1) == '\0' || *(argv + 2) == '\0')
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
 		printf("%d\n", (atoi(*(argv + 1)) * atoi(*(argv + 2))));
 	return (0);
