bbe32d78ef9f7a57cf200f9960a20b183b8883ca
Juan Sebastian <juanllano93@gmail.com>
2020-03-01 18:29:45 -0500

    add task adv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,11 +13,9 @@ int main(int argc, char *argv[])
 {
 	int values[] = {25, 10, 5, 2, 1}, i, j, result = 0, n;
 
-
 	if (argc == 2)
 	{
 		n = atoi(argv[1]);
-
 		for (i = 0; i < 5; i++)
 		{
 			if (n < 0)
@@ -28,21 +26,16 @@ int main(int argc, char *argv[])
 			if (n == 0)
 			{
 				printf("%d\n", result);
-				result += 0;
 				return (0);
 			}
-
 			if (values[i] <= n)
 			{
 				result += n / values[i];
 				n = abs(n - (values[i] * result));
 			}
-			else
-				result += 0;
-
 			for (j = 0; j < 5; j++)
 			{
-				if ( n == values[j])
+				if (n == values[j])
 				{
 					result += 1;
 					n = 0;
@@ -55,6 +48,5 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
 }
