315ae333695db8fc9dc0c1dc30e2026fe0e09b35
Juan Sebastian <juanllano93@gmail.com>
2020-03-04 02:23:00 +0000

    add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to String
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+/**
+ * str_concat - function that concatenates two strings.
+ * @s1: char to concatenate
+ * @s2: char to concatenate
+ *
+ * Return: pointer to char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *str, lenght = 0, lenght1 = 0, lenght2 = 0, i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	lenght1 = _strlen(s1);
+	lenght2 = _strlen(s2);
+	lenght = lenght1 + _strlen(s2) + 1;
+	str = malloc(lenght * sizeof(char));
+
+	for (i = 0; i < lenght1; i++)
+		*(str + i) = *(s1 + i);
+
+	for (i = 0; i <= lenght2; i++)
+		*(str + i + lenght1) = *(s2 + i);
+
+	return (str);
+}
