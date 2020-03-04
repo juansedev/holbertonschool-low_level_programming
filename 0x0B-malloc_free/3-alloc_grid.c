cceca3afc3d39cefc4489a4c391096be8a6e9b78
Juan Sebastian <juanllano93@gmail.com>
2020-03-04 04:49:34 +0000

    add task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,8 +19,6 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < width; i++)
 		array[i] = (int *)malloc(height * sizeof(int));
 
-	if (array == NULL)
-		return (NULL);
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
