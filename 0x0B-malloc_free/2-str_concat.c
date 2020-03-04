f5ec60d340cdb216384ea27b0419878b17d796f5
Juan Sebastian <juanllano93@gmail.com>
2020-03-04 02:27:25 +0000

    add task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,8 +29,11 @@ char *str_concat(char *s1, char *s2)
 {
 	char *str, lenght = 0, lenght1 = 0, lenght2 = 0, i;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 ="";
+
+	if (s2 == NULL)
+		s2 ="";
 
 	lenght1 = _strlen(s1);
 	lenght2 = _strlen(s2);
