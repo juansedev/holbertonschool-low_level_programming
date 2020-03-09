3aa6f95e9da05adb9281693b268673b7fe2a923a
Juan Sebastian <juanllano93@gmail.com>
2020-03-09 23:11:58 +0000

    add task 2

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	struct dog my_dog;
 
-	my_dog.name = "Django";
-	my_dog.age = 3.5;
-	my_dog.owner = "Jay";
+	my_dog.name = NULL;
+	my_dog.age = -1;
+	my_dog.owner = NULL;
 	print_dog(&my_dog);
 	return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,14 +13,14 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: nil\n");
+			printf("Name: (nil)\n");
 		if (d->age > 0)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: nil\n");
+			printf("Age: (nil)\n");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: nil\n");
+			printf("Owner: (nil)\n");
 	}
 }
