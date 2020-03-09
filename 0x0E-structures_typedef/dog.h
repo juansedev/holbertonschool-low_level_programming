fcd3f22acead7bd008349f3201417807bef74753
Juan Sebastian <juanllano93@gmail.com>
2020-03-09 22:09:12 +0000

    add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Struct to define a dog
+ * @name: name of dog
+ * @owner: name's owner of dog
+ * @age: years old
+ *
+ * Description: Structure dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
