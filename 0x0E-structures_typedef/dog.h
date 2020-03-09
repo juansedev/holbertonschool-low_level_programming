49c6e2c6b2384d3738165cab073a6e0ceb0b742c
Juan Sebastian <juanllano93@gmail.com>
2020-03-09 23:20:00 +0000

    add task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,12 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 int _putchar(char c);
 
