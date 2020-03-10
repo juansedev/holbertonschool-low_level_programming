59fe89eb5ba71e53cee59bfc1208977946160bc3
Juan Sebastian <juanllano93@gmail.com>
2020-03-10 00:45:44 +0000

    add task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,27 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strcpy - function that copies the string pointed to
+ * @dest: pointer to string entry data
+ * @src: destiny data
+ * Return: return value of dest
+ */
+
+void _strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (src != '\0')
+	{
+		for (i = 0;  *(src + i) != '\0'; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
+	}
+
+	*(dest + i) = '\0';
+}
+/**
  * _strlen - returns the length of a string.
  * @s: pointer to String
  * Return: Nothing
@@ -29,24 +50,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct dog *n_dog;
 	char *cp_name;
 	char *cp_owner;
-	
-	if (name == NULL)
-	{
-		free(name);
-		return (NULL);
-	}
-	cp_name = name;
 
-	if (owner == NULL)
-	{
-		free(owner);
+	cp_name = malloc((_strlen(name) + 1) * sizeof(char));
+	if (cp_name == NULL)
 		return (NULL);
-	}
-	cp_owner = owner;
+
+	cp_owner = malloc((_strlen(owner) + 1) * sizeof(char));
+	if (cp_name == NULL)
+		return (NULL);
+	
+	_strcpy(cp_name, name);
+	_strcpy(cp_owner, owner);
 
 	n_dog =  malloc(sizeof(struct dog));
 	if (n_dog == NULL)
 		return (NULL);
+
 
 	n_dog->name = name;
 	n_dog->age = age;
