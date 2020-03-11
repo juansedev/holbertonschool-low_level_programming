35ca5248517bcd0c83fd7e900897ebd409c47937
Juan Sebastian <juanllano93@gmail.com>
2020-03-11 19:55:24 +0000

    add task 1 and update header file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function given as a parameter
+ * on each element of an array
+ * @array: point to array of integers
+ * @size: lenght of array input
+ * @action: pointer to function that prints each element of an array
+ *
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (action)
+	{
+		while (i < size)
+		{
+			(*action)(array[i]);
+			i++;
+		}
+	}
+}
