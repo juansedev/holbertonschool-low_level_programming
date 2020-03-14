234ec1319a4cbb1b730802e0544ee8a3a57ba898
Juan Sebastian <juanllano93@gmail.com>
2020-03-14 05:30:56 +0000

    add update 3

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_strings(", ", 5, "Jay", "Django", "dasd", "adad", "adad");
+	return (0);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,19 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct fmt - Struct fmt
+ *
+ * @op: The format
+ * @f: The function associated
+ */
+typedef struct str_fmt
+{
+	char *inp_fmt;
+	void (*f)(va_list list);
+} fmt_a;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
