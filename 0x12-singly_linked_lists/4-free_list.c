039af91ea6c473b9f14b7456a84c1d8d94dd58f7
Juan Sebastian <juanllano93@gmail.com>
2020-03-20 23:19:08 +0000

    add task 4 and update heade

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: Pointed to head of a list
+ *
+ * Return: The number of nodes
+ */
+void free_list(list_t *head)
+{
+
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+	free_list(head->next);
+	free(head->next);
+	free(head->str);
+	free(head);
+}
