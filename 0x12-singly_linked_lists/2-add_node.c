635b6f33b9a965e64bf97fc946b9126b0618f18c
Juan Sebastian <juanllano93@gmail.com>
2020-03-20 20:37:54 +0000

    add task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,18 +23,18 @@ int _strlen(const char *s)
  */
 
 list_t *add_node(list_t **head, const char *str)
+
 {
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (new_node != NULL)
 	{
+		new_node->str = strdup(str);
+		new_node->len = _strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+	} else
 		return (NULL);
-	}
-	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-
 	return (new_node);
 }
