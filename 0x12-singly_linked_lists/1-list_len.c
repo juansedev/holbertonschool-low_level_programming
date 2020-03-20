b494a2aa4fb335285fd5dea56fcc46cd4d07d31f
Juan Sebastian <juanllano93@gmail.com>
2020-03-20 00:48:23 +0000

    add task 2 - count nodes of a linked list recursively

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: Pointed to head of a list
+ *
+ * Return: The number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int nodes = 1;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+    nodes += list_len((h->next));
+
+	return (nodes);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,5 +21,6 @@ typedef struct list_s
 } list_t;
 /* Prototypes */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /*lists.h*/
