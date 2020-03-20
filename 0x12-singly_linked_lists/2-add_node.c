5b4c152f5560220987c8274e9905636b6d56baaa
Juan Sebastian Llano Gallego <38362436+EckoJuan@users.noreply.github.com>
2020-03-19 22:58:28 -0500

    Update 2-add_node.c
    .

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,9 +31,6 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
-	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
