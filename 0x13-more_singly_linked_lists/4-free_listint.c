e92addc55dfcd1f166acc746cd44aa7150cc31e0
Juan Sebastian <juanllano93@gmail.com>
2020-03-23 19:51:29 +0000

    add task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: Pointed to head of a list
+ *
+ * Return: Void
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+	free_listint(head->next);
+	free(head);
+}
