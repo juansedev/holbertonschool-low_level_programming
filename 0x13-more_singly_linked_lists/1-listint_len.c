498fb608be580aa723846419b350139edf2c7230
Juan Sebastian <juanllano93@gmail.com>
2020-03-23 17:05:50 +0000

    add task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint - function that prints all the elements of a listint_t list.
+ * listint_len - function that prints all the elements of a listint_t list.
  * @h: Is a first structure of a linked list
  *
  * Return: The number of nodes
