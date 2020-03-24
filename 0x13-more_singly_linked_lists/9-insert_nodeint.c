f51f8d977d95d6a7e0f3cc1d3affed20c2680c15
Juan Sebastian <juanllano93@gmail.com>
2020-03-24 20:12:55 +0000

    add 9 task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	/*new_node->next = NULL;*/
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
