54dd5e7cef488ec473a74784bf6c1c11be72b2d7
Juan Sebastian <juanllano93@gmail.com>
2020-03-26 23:56:27 +0000

    add task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ * _pow - function to do a pow
+ * @base: base of potency
+ * @power: exponent
+ *
+ * Return: value of potency
+ */
 unsigned int _pow(int base, unsigned int power)
 {
 	unsigned int result = 1, i;
@@ -13,10 +19,10 @@ unsigned int _pow(int base, unsigned int power)
 }
 
 /**
- * binary_to_unit - function that converts a binary number to an unsigned int
+ * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: is pointing to a string of 0 and 1 chars
- * 
- * Return: 
+ *
+ * Return: Value in decimal
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -25,6 +31,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == '\0')
 		return (0);
+
 	while (b[lenght])
 	{
 		if (b[lenght] != '1' && b[lenght] != '0')
@@ -36,12 +43,11 @@ unsigned int binary_to_uint(const char *b)
 
 	i = lenght - 1;
 	lenght = 0;
-	for ( ;i >= 0; --i)
+	for ( ; i >= 0; --i)
 	{
 		if (b[i] == '1')
 		{
-			decimal = decimal + _pow(2,lenght);
-
+			decimal = decimal + _pow(2, lenght);
 		}
 		lenght++;
 	}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -3,7 +3,7 @@
 
 
 unsigned int binary_to_uint(const char *b);
-unsigned int _pow(int base, unsigned int power);
+/*unsigned int _pow(int base, unsigned int power);*/
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
