10de1cc71be2f031afdd497663532a5215772f65
Juan Sebastian <juanllano93@gmail.com>
2020-03-26 23:23:52 +0000

    add task 0 again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,14 +2,14 @@
 
 unsigned int _pow(int base, unsigned int power)
 {
-    unsigned int result = 1, i;
+	unsigned int result = 1, i;
 
-    for (i = 0; i < power; i++)
-    {
-        result = result * base;
-    }
+	for (i = 0; i < power; i++)
+	{
+		result = result * base;
+	}
 
-    return (result);
+	return (result);
 }
 
 /**
@@ -20,28 +20,30 @@ unsigned int _pow(int base, unsigned int power)
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int lenght = 0, decimal = 0;
-    int i;
+	unsigned int lenght = 0, decimal = 0;
+	int i;
 
-    while (b[lenght])
-    {
-        if (b[lenght] != '1' && b[lenght] != '0')
-        {
-            return (0);
-        }
-        lenght++;
-    }
+	if (b == '\0')
+		return (0);
+	while (b[lenght])
+	{
+		if (b[lenght] != '1' && b[lenght] != '0')
+		{
+			return (0);
+		}
+		lenght++;
+	}
 
-    i = lenght - 1;
-    lenght = 0;
-    for ( ;i >= 0; --i)
-    {
-        if (b[i] == '1')
-        {
-            decimal = decimal + _pow(2,lenght);
+	i = lenght - 1;
+	lenght = 0;
+	for ( ;i >= 0; --i)
+	{
+		if (b[i] == '1')
+		{
+			decimal = decimal + _pow(2,lenght);
 
-        }
-        lenght++;
-    }
-    return (decimal);
+		}
+		lenght++;
+	}
+	return (decimal);
 }
