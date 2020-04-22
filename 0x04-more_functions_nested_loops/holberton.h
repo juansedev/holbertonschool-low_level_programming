d9c66b4b653bb27d2fa5042f8d87edf4268744c8
Juan Sebastian Llano Gallego <38362436+llanojs@users.noreply.github.com>
2020-04-22 15:27:49 -0500

    Update Header file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -11,15 +11,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 void print_square(int size);
 void print_triangle(int size);
->>>>>>> 392d491848f636c4e835156c05c1cfd685354ece
-=======
-void print_square(int size);
-void print_triangle(int size);
->>>>>>> 392d491848f636c4e835156c05c1cfd685354ece
 
 #endif /* HOLBERTON_H */
