2bc91829fa05b6f34f958749cbca87f389580a32
Juan Sebastian <juanllano93@gmail.com>
2020-05-02 22:00:40 +0000

    add task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - Function that count all the elements
+ * delete_dnodeint_at_index - Function that count all the elements
  * of a dlistint_t list
  * @head: double  Pointer with the addres of head of a list
  * @index: value of the index
@@ -9,12 +9,12 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-        dlistint_t *copy = *head;
+	dlistint_t *copy = *head;
 	unsigned int ind_node = 0;
 
-        if (!head || !(*head))
+	if (!head || !(*head))
 		return (-1);
-                
+
 	if (index == 0)
 	{
 		free(*head);
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 	while (copy)
-	{	
+	{
 		if (ind_node == index)
 		{
 			(copy->prev)->next = (copy->next);
@@ -32,25 +32,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 		copy = copy->next;
 		ind_node++;
-
-		/*
-		ind_node = 0;
-		while (ind_node != index)
-		{
-			copy = copy->next;
-			ind_node++;
-		}
-                if (index != ind_node - 1)
-                        (copy->prev)->next = (copy->next);
-                else
-                        (copy->prev)->next = NULL;
-                if (index != 0)
-                        (copy->next)->prev = (copy->prev);
-                else
-                        (copy->next)->prev = NULL;
-                free(copy);
-                return (1);
-		*/
-	} 
+	}
 	return (-1);
 }
