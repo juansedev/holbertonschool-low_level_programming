d0b308a70064e8bf0fb5320d3e44ff7a3d607ad8
Juan Sebastian <juanllano93@gmail.com>
2020-05-03 01:25:14 +0000

    add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,15 +13,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *copy = *h, *insert;
 	unsigned int ind_node = 0;
 
-	if (!h || !(*h))
+	if (!h)
 		return (NULL);
-
 	insert = malloc(sizeof(dlistint_t));
 	if (insert == NULL)
 		return (NULL);
-
 	insert->n = n;
-
+	insert->prev = NULL;
+	if (idx == 0 && copy->next == NULL)
+	{
+		insert->next = NULL;
+		*h = insert;
+		return (insert);
+	}
+	if (idx == 0 && copy->next != NULL)
+	{
+		insert->next = copy;
+		*h = insert;
+		return (insert);
+	}
 	while (copy)
 	{
 		if (ind_node == idx && copy->next != NULL)
@@ -34,10 +44,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		if (ind_node == idx && copy->next == NULL)
 		{
-			(copy->prev)->next = insert;
 			insert->prev = copy->prev;
-			(copy->next)->prev = insert;
+			(copy->prev)->next = insert;
 			insert->next = copy;
+			return (insert);
+		}
+		if (ind_node + 1 == idx && copy->next == NULL)
+		{
+			insert->next = NULL;
+			insert->prev = copy;
+			copy->next  = insert;
 			return (insert);
 		}
 		copy = copy->next;
