8ad125b065e0e47fbe1c7849e4c89d8a3d970d93
Juan Sebastian <juanllano93@gmail.com>
2020-05-04 03:21:35 +0000

    add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,54 +10,61 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *copy = *h, *insert;
-	unsigned int ind_node = 0;
+	dlistint_t *insert = NULL, *copy = *h;
+	unsigned int ind_node = 0, len = 0;
 
-	if (!h)
-		return (NULL);
-	insert = malloc(sizeof(dlistint_t));
-	if (insert == NULL)
-		return (NULL);
-	insert->n = n;
-	insert->prev = NULL;
-	if (idx == 0 && copy->next == NULL)
+	len = dlistint_len(*h);
+
+	if (idx <= len)
 	{
-		insert->next = NULL;
-		*h = insert;
+		if (idx == 0)
+			insert = add_dnodeint(h, n);
+		else if (idx == len)
+			insert = add_dnodeint_end(h, n);
+		else
+		{
+			insert = malloc(sizeof(dlistint_t));
+			if (!insert)
+				return (NULL);
+			insert->n = n;
+			while (copy)
+			{
+				if (idx - 1 == ind_node)
+				{
+					insert->next = copy->next;
+					(copy->next)->prev = insert;
+					copy->next = insert;
+					insert->prev = copy;
+					return (insert);
+				}
+				ind_node++;
+				copy = copy->next;
+			}
+		}
 		return (insert);
-	}
-	if (idx == 0 && copy->next != NULL)
-	{
-		insert->next = copy;
-		*h = insert;
-		return (insert);
-	}
-	while (copy)
-	{
-		if (ind_node == idx && copy->next != NULL)
-		{
-			(copy->prev)->next = insert;
-			insert->prev = copy->prev;
-			(copy->next)->prev = insert;
-			insert->next = copy;
-			return (insert);
-		}
-		if (ind_node == idx && copy->next == NULL)
-		{
-			insert->prev = copy->prev;
-			(copy->prev)->next = insert;
-			insert->next = copy;
-			return (insert);
-		}
-		if (ind_node + 1 == idx && copy->next == NULL)
-		{
-			insert->next = NULL;
-			insert->prev = copy;
-			copy->next  = insert;
-			return (insert);
-		}
-		copy = copy->next;
-		ind_node++;
 	}
 	return (NULL);
+}
+/**
+ * dlistint_len - Function that count all the elements of a dlistint_t list
+ * @h: Pointed to head of a list
+ *
+ * Return: The number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *tmp = h;
+	size_t nodes = 0;
+
+	if (tmp == NULL)
+	{
+		return (0);
+	}
+
+	while (tmp)
+	{
+		nodes++;
+		tmp = tmp->next;
+	}
+	return (nodes);
 }
