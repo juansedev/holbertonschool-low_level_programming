adb7e2355b5b610cebb78eb621ffb5023974cf0a
Juan Sebastian Llano <juanllano93@gmail.com>
2020-05-30 04:59:47 +0000

    add task 3 fixed betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,11 +9,11 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        hash_node_t *new_node = NULL, *tmp = NULL;
+	hash_node_t *new_node = NULL, *tmp = NULL;
 	unsigned long int index_k = 0;
 
-        if (ht == NULL || key == NULL || value == NULL)
-		return (0);
+	if (ht == NULL || key == NULL || value == NULL)
+	return (0);
 
 	if (ht)
 	{
@@ -41,5 +41,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		return (1);
 	}
-        return (0);
+	return (0);
 }
