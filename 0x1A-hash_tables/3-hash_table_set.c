45aeb3b5e12b5d880c7efbedae6948b6a413fd67
Juan Sebastian Llano <juanllano93@gmail.com>
2020-05-30 04:47:18 +0000

    add task 5

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+        hash_node_t *key_n = NULL, *tmp = NULL;
+	unsigned long int index = 0;
+
+	if (ht)
+	{
+
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print -function that prints a hash table.
+ * @ht: hash table.
+ * Return:void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+        hash_node_t *tmp = NULL;
+	unsigned long int index;
+        unsigned int i = 0;
+
+	if (ht)
+        {
+                printf("{");
+                for (index = 0; index < ht->size; index++)
+                {
+                        tmp = ht->array[index];
+                        while (tmp != NULL)
+                        {
+                                if (i == 1)
+                                        printf(", ");
+                                printf("'%s': '%s'", tmp->key, tmp->value);
+                                i = 1;
+                                tmp = tmp->next;
+                        }
+                }
+                printf("}\n");
+        }
+}
