f11a9767ecfbd15294d2a352d9c9b6c8ed9c4743
Juan Sebastian Llano <juanllano93@gmail.com>
2020-05-30 04:55:37 +0000

    add task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,13 +9,37 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        hash_node_t *key_n = NULL, *tmp = NULL;
-	unsigned long int index = 0;
+        hash_node_t *new_node = NULL, *tmp = NULL;
+	unsigned long int index_k = 0;
+
+        if (ht == NULL || key == NULL || value == NULL)
+		return (0);
 
 	if (ht)
 	{
+		index_k = key_index((unsigned char *)key, ht->size);
+
+		tmp = ht->array[index_k];
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+
+		new_node = malloc(sizeof(hash_node_t));
+		if (!new_node)
+			return (0);
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		new_node->next = ht->array[index_k];
+		ht->array[index_k] = new_node;
 
 		return (1);
 	}
-	return (0);
+        return (0);
 }
