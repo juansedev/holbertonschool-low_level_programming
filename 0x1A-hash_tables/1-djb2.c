522d4b8a122cff4a521c6d8fc1a3fba2a8f5c169
Juan Sebastian Llano <juanllano93@gmail.com>
2020-06-12 08:08:17 +0000

    add task 4

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -13,7 +13,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	hash = 5381;
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = hash * 33 + c; /*((hash << 5) + hash) + c */
 	}
 	return (hash);
 }
