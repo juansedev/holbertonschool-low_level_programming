7959b1b7d65bc8e61c81a201acb81e480ef0b094
Juan Sebastian Llano <juanllano93@gmail.com>
2020-08-08 14:31:30 +0000

    add make

---
+++ 0x1C-makefiles/5-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+5-main
+"""
+island_perimeter = __import__('5-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [[]]
+    print(island_perimeter(grid))
